444c486efb946d8e1449db37a6847134bb6594a5
Tamim Mahmud <143133639+tamim1249@users.noreply.github.com>
2026-06-11 01:45:10 +0600

    Create 01_setup_imports.py

---
+++ 01_setup_imports.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import os
+import kagglehub
+
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
